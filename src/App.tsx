08e013ba9bd499edb20261bd6eb7f13063eccbc3
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-09-01 12:50:01 +0300

    init packages

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { InputField } from "./components/InputField/InputField"
+
+function App() {
+  return (
+    <div className="flex justify-center bg-slate-500 p-2">
+      <InputField />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,5 @@
+export const InputField = () => {
+  return(
+    <input type="text" className="p-2 w-full" />
+  )
+}
